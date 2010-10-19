b0c356d15a4e57c7cb8f7786470bcf26b9d47f8d
jonauer <jonauer@69916375-d940-0410-a7c5-0f21dd258057>
2010-10-19 22:27:51 +0000

    I think I have the open server working now. It does not update the database stats on a successful or failed registration but it should be functional

--- src/open-bam-daemon.pl
+++ src/open-bam-daemon.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/perl -w
-# JungleAuth drop in replacement for Canopy BAM daemon
+# Drop in replacement for Canopy BAM 2.0 daemon
 #
 #  Copyright 2010 Jonathan Auer
 #  Licensed under the Apache License, Version 2.0 (the "License");
@@ -19,13 +19,14 @@ use warnings;
 use strict;
 
 use POE;
-use POE::Component::EasyDBI;
+use POE::Component::SimpleDBI;
 
 use IO::Socket::INET;
 use Net::Canopy::BAM;
 
 use Config::INI::Reader;
 
+use Tie::Hash;
 use Data::Dumper;
 
 # Handle startup init tasks
@@ -37,14 +38,87 @@ if (@ARGV != 1) {
 my $config = Config::INI::Reader->read_file($ARGV[0]);
 $config = $config->{client};
 
+
+my $ncb = Net::Canopy::BAM->new();
+POE::Component::SimpleDBI->new('SimpleDBI') or die 'Unable to create DBI session';
+
 POE::Session->create(
   inline_states => {
-    _start       => \&server_start,
-    get_datagram => \&server_read,
+    _start => sub {
+      $_[KERNEL]->post('SimpleDBI', 'CONNECT',
+        'DSN'         => "DBI:mysql:database=04004SSE;host=$config->{sqlhost};port=3306",
+        'USERNAME'    => $config->{user},
+        'PASSWORD'    => $config->{password},
+        'NOW'         => 1,
+        'EVENT'       => 'conn_handler',
+        'AUTO_COMMIT' => 0,
+      );
+    },
+
+    'conn_handler' => sub {
+      my ($kernel, $val) = @_[KERNEL, ARG0];
+      if ($val->{ERROR}) {
+        print "Fatal error with database: $val->{ERROR}\n";
+        exit(1);
+      };
+    }
+  },
+);
+
+# Network handler
+POE::Session->create(
+  inline_states => {
+    _start        => \&server_start,
+    get_datagram  => \&server_read,
+    auth_request  => \&auth_request,
+    auth_response => \&auth_response,
   }
 );
 POE::Kernel->run();
 exit;
+
+# Generate authentication response
+sub auth_response {
+  my ($kernel, $dbres) = @_[KERNEL, ARG0];
+  my $data = delete($dbres->{BAGGAGE});
+
+  my $resp;
+
+  if ($dbres->{RESULT}) {
+    $resp = $ncb->mkAcceptPacket(
+      seq => $data->{seq},
+      mac => $data->{sm},
+      qos => $dbres->{RESULT}->{qos},
+    );
+  } else {
+    $resp = $ncb->mkRejectPacket(
+      seq => $data->{seq},
+      mac => $data->{sm},
+    );
+  }
+
+  my $respsock = new IO::Socket::INET->new(
+    PeerPort  => 61001,
+    Proto     => 'udp',
+    PeerAddr  => $data->{apip},
+    LocalPort => 61001,
+  ) or warn "Could not create reply socket";
+  
+  if ($respsock) {
+    $respsock->send($resp);
+  }
+}
+
+# Handle authentication request
+sub auth_request {
+  my ($kernel, $data) = @_[KERNEL, ARG0];
+  $kernel->post('SimpleDBI', 'SINGLE',
+    'SQL'          => 'SELECT qos FROM SS WHERE esn = ?',
+    'PLACEHOLDERS' => [$data->{sm}],
+    'EVENT'        => 'auth_response',
+    'BAGGAGE'      => $data,
+  );
+}
 
 # Setup server connection
 sub server_start {
@@ -59,5 +133,15 @@ sub server_start {
 
 sub server_read {
   my ($kernel, $socket) = @_[KERNEL, ARG0];
+  my $data;
+
+  $socket->recv($data, 20);
+  my $apIP = $socket->peerhost();
+
+  $data = $ncb->parsePacket(packet => $data);
+  if ($data->{type} eq 'authreq') {
+      $data->{apip} = $apIP;
+      $kernel->post($_[SESSION], 'auth_request', $data);
+  }
 }
 
