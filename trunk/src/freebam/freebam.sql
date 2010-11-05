14e09a7dfe3e795a10ae14c4ac60ee5240a58578
jda@tapodi.net <jda@tapodi.net@69916375-d940-0410-a7c5-0f21dd258057>
2010-11-05 04:14:13 +0000

    started work on free(dom) BAM, no dependencies on Moto

---
+++ trunk/src/freebam/freebam.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `sm` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `mac` varchar(16) NOT NULL,
+  `sup` int(5) NOT NULL,
+  `sdown` int(5) NOT NULL,
+  `bup` int(6) NOT NULL,
+  `bdown` int(6) NOT NULL,
+  `active` int(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique_mac` (`mac`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
